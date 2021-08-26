6f2f6473a91425e88a4eb93319ea9c0cbd5a9c40
tanupoo <github-shoichi@tanu.org>
2021-08-26 21:20:40 +0900

    added switch whether to do npm install.

--- build.sh
+++ build.sh
@@ -5,14 +5,26 @@ build()
     (
         target=$1
         echo "==== Building $target ===="
-        cd ui/$target
-        npm install
-        patch -N -p 0 -i ../vue-cli.patch
+        cd $target
+        if [ ${do_npm_install} -eq 1 ] ; then
+            npm install
+            patch -N -p 0 -i ../vue-cli.patch
+        fi
+        if [ ! -d "node_modules" ] ; then
+            echo "ERROR: no node modules.  Rerun with the -n option."
+            exit 0
+        fi
         alias vue=`pwd`/node_modules/@vue/cli/bin/vue.js
         cp -r ../common src/
         npm run build
     )
 }
+
+do_npm_install=0
+if [ "$1" = "-n" ] ; then
+    do_npm_install=1
+    shift
+fi
 
 case "$1" in
     1) build step1 ;;
